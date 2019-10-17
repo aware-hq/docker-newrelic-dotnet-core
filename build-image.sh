8013cf57caad65c96fa1ebac8caebf2985d2c1a8
KyJah Keys <kyjah.keys@gmail.com>
2019-10-17 15:52:37 -0400

    updated image tagging to stop overwriting aspnet images

--- build-image.sh
+++ build-image.sh
@@ -5,14 +5,14 @@
 docker build \
   --build-arg NewRelicVersion=${NEW_RELIC_VERSION} \
   --build-arg NewRelicUrl=${NEW_RELIC_URL} \
-  --build-arg CONTAINER_IMAGE=${CONTAINER_IMAGE}:${IMAGE_TAG} \
+  --build-arg CONTAINER_IMAGE=${CONTAINER_IMAGE}${IMAGE_RUNTIME}:${IMAGE_TAG} \
   --build-arg VERSION=$(git describe --tags --always) \
   --build-arg COMMIT=$(git rev-parse HEAD) \
   --build-arg URL=$(git config --get remote.origin.url) \
   --build-arg BRANCH=$(git rev-parse --abbrev-ref HEAD) \
   --build-arg DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
-  -f $DOCKERFILE_PATH -t $IMAGE_NAME:${IMAGE_TAG} .
+  -f $DOCKERFILE_PATH -t $IMAGE_NAME/${IMAGE_RUNTIME}:${IMAGE_TAG} .
 if [[ "$TRAVIS_BRANCH" == "master" ]]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push ${IMAGE_NAME}:${IMAGE_TAG}
+  docker push ${IMAGE_NAME}/${IMAGE_RUNTIME}:${IMAGE_TAG}
 fi
