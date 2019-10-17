06647c772943fb7b0af5e7be01dda4f4ed84e3e6
KyJah Keys <kyjah.keys@gmail.com>
2019-10-17 19:33:14 -0400

    completed merge

--- build-image.sh
+++ build-image.sh
@@ -5,25 +5,14 @@
 docker build \
   --build-arg NewRelicVersion=${NEW_RELIC_VERSION} \
   --build-arg NewRelicUrl=${NEW_RELIC_URL} \
-<<<<<<< HEAD
   --build-arg CONTAINER_IMAGE=${CONTAINER_IMAGE}${IMAGE_RUNTIME}:${IMAGE_TAG} \
-=======
-  --build-arg CONTAINER_IMAGE=${CONTAINER_IMAGE}:${IMAGE_TAG} \
->>>>>>> c7f96302a51a664564dff8f52e98b12bdb2fe42b
   --build-arg VERSION=$(git describe --tags --always) \
   --build-arg COMMIT=$(git rev-parse HEAD) \
   --build-arg URL=$(git config --get remote.origin.url) \
   --build-arg BRANCH=$(git rev-parse --abbrev-ref HEAD) \
   --build-arg DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
-<<<<<<< HEAD
   -f $DOCKERFILE_PATH -t $IMAGE_NAME/${IMAGE_RUNTIME}:${IMAGE_TAG} .
 if [[ "$TRAVIS_BRANCH" == "master" ]]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
   docker push ${IMAGE_NAME}/${IMAGE_RUNTIME}:${IMAGE_TAG}
-=======
-  -f $DOCKERFILE_PATH -t $IMAGE_NAME:${IMAGE_TAG} .
-if [[ "$TRAVIS_BRANCH" == "master" ]]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
-  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push ${IMAGE_NAME}:${IMAGE_TAG}
->>>>>>> c7f96302a51a664564dff8f52e98b12bdb2fe42b
 fi
