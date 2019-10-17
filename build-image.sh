00f0e0637f68a63ace79cf47516a25e580ae1e7f
KyJah Keys <kyjah.keys@gmail.com>
2019-10-17 15:25:12 -0400

    dropped build matrix

--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+DOCKERFILE_PATH=${1}
+CONTAINER_IMAGE=${2}
+IMAGE_TAG=${3}
 docker build \
   --build-arg NewRelicVersion=${NEW_RELIC_VERSION} \
   --build-arg NewRelicUrl=${NEW_RELIC_URL} \
@@ -9,7 +12,7 @@ docker build \
   --build-arg BRANCH=$(git rev-parse --abbrev-ref HEAD) \
   --build-arg DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
   -f $DOCKERFILE_PATH -t $IMAGE_NAME:${IMAGE_TAG} .
-if [[ "$TRAVIS_BRANCH" == "master" ]]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
-  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push ${IMAGE_NAME}:${IMAGE_TAG}
-fi
+#if [[ "$TRAVIS_BRANCH" == "master" ]]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
+#  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+#  docker push ${IMAGE_NAME}:${IMAGE_TAG}
+#fi
