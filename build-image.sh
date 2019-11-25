aec376113698c4e274b37ee51354fd6c2a404353
KyJah Keys <6628005+mightyshazam@users.noreply.github.com>
2019-11-25 13:43:35 -0500

    removed date form build script

--- build-image.sh
+++ build-image.sh
@@ -10,7 +10,6 @@ docker build \
   --build-arg COMMIT=$(git rev-parse HEAD) \
   --build-arg URL=$(git config --get remote.origin.url) \
   --build-arg BRANCH=$(git rev-parse --abbrev-ref HEAD) \
-  --build-arg DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
   -f $DOCKERFILE_PATH -t $IMAGE_NAME-${IMAGE_RUNTIME}:${IMAGE_TAG} .
 if [[ "$TRAVIS_BRANCH" == "master" ]]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
